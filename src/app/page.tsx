f04b8ca2d497041eb1dc834abb54e656d84ca83f
abhishek04012005 <abhishekchoudhary5414@gmail.com>
2025-08-24 14:09:01 +0530

    Blog added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Hero from "@/components/hero/Hero";
 import About from "@/components/about/About";
-// import Work from "@/components/work/Work";
 import Contact from "@/components/contact/Contact";
 import Project from "@/components/project/Project";
 import Services from "@/components/service/Service";
 import Testimonial from "@/components/testimonial/Testimonial";
+import Blog from "@/components/blog/Blog";
 
 
 export default function Home() {
@@ -14,6 +14,7 @@ export default function Home() {
       <About />
       <Services />
       <Project />
+      <Blog />
       <Testimonial />
       <Contact />
     </div>
